a26dbf1ac6d750635b918ab2e8d256811efad8de
Евгений <Klimber43@gmail.com>
2023-12-26 21:53:54 +0400

    Java3.Lesson1

---
+++ src/main/java/Java3/Lesson1/Apple.java
@@ -0,0 +1,8 @@
+package Java3.Lesson1;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        super(1.0);
+    }
+}
---
+++ src/main/java/Java3/Lesson1/Fruit.java
@@ -0,0 +1,14 @@
+package Java3.Lesson1;
+
+public class Fruit {
+
+    protected double weight;
+
+    public Fruit(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
